7aa67e1895cfa52cbc3db6bbf9d1da479e9428d1
wrappixel <info@wrappixel.com>
2021-12-02 16:13:48 +0530

    ''

--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -89,6 +89,15 @@ const Sidebar = () => {
               </Link>
             </NavItem>
           ))}
+          <Button
+            color="danger"
+            tag="a"
+            target="_blank"
+            className="mt-3"
+            href="https://wrappixel.com/templates/monster-react-admin/?ref=33"
+          >
+            Upgrade To Pro
+          </Button>
         </Nav>
       </div>
     </div>
