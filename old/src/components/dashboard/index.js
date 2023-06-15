a6e36646899bc6fcd2f350cc21f82a0177da07ac
wrappixelTeam <wrappixelteam.2016@gmail.com>
2023-06-15 15:12:00 +0530

    ''

--- old/src/components/dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import TotalRevenue from './total-revenue/total-revenue.jsx';
-import Projects from './projects/projects.jsx';
-import Feeds from './feeds/feeds.jsx';
-
-export {
-    TotalRevenue,
-    Projects,
-    Feeds
-};
